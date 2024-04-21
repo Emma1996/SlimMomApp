9bfa5adbda0fb6ef35da4ffa57a2470d2559aee6
Emma1996 <mocanpetruta96@gmail.com>
2024-04-21 22:35:45 +0300

    commit

---
+++ services/api/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://slimmom-oz0k.onrender.com';
+//axios.defaults.baseUrl = 'http://localhost:3000';
+//const API_KEY = 'e8938dc969e4885481c1163e17374d0f';
+
+const END_POINTS = {
+  DailyCalorieIntake: '/api/products',
+};
+
+export const apiCalorieIntak = async () => {
+  const res = await axios.get(`${END_POINTS.DailyCalorieIntake}`);
+  return res.data.results;
+};
+//export const apiCalorieIntake = async body => {
+// const res = await axios.post(END_POINTS.DailyCalorieIntake, body);
+//console.log('resData', res.data);
+//return res.data;
+//};
